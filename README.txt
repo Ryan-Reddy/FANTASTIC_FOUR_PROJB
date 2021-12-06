7398c1a9f4dcb09b56225ded68a78a762201ace5
“klugook” <ryanreddy@hotmail.com>
2021-12-06 17:32:24 +0100

    ✓ bronbestand inladen ✓ functie die een GUI weergeeft ✓ Een functie die de naam van het eerste spel uit het bronbestand weergeeft in het dashboard ✓ Een functie waarmee de data uit het bronbestand gesorteerd kan worden (standaard sorteerfunctie van Python)

--- README.txt
+++ README.txt
@@ -2,15 +2,19 @@
 
 Een project om steam mogelijkheden uit te breiden door middel van get tracken van vrienden.
 
+Ryan v Lil, klugook,:
+steam.json contains:  541302  records,
+divided over  17348935  lines.
+
 -----------------------------------------------------------------------------------------------------------------------
 
 DEV_AZURE:
 user: <unchangable> (hbo-ict//)
-pw team_repo = be6q4usriamzyph4bnyewy4qkqw2tew7345bsg5ey3wqnkbxuira
+pw team_repo = be6q4usriamzyph4bnye wy4qkqw2tew7345bsg5ey3wqnkbxuira
 
 -----------------------------------------------------------------------------------------------------------------------
-steam.json contains:  541302  records,
-divided over  17348935  lines.
+Packages used:
+* tkinter(standard python3)
 
 
 
--- main.py
+++ main.py
@@ -1,8 +1,58 @@
-print('hello worlrld'.format())
+# Imported Packages:
+import json
+from tkinter import *
 
-num = sum(1 for element in open('steam.json'))
-print('steam.json contains: ', num, ' records,')
+# Functions:
+def startup_message():
+    print('hello worlrld'.format())
+    num = sum(1 for element in open('steam.json'))
+    print('steam.json contains: ', num, ' records,')
 
-stream = open('steam.json', 'r')
-steam = stream.read()
-print('divided over ',len(steam), ' lines.')
+    stream = open('steam.json', 'r')
+    steam = stream.read()
+    print('divided over ',len(steam), ' lines.')
+
+def GUI_function():
+    root = Tk()
+    root.geometry('1024x720')
+    root.title('Steam App Fantastic Five')
+    root['bg'] = '#393D47'
+    root.wait_visibility(root)
+    root.wm_attributes('-alpha', 0.9)
+
+    L1 = Label(root, text="First game in list:", background='#393D47', foreground='white').grid(column=2, row=1)
+    L2 = Label(root, text=first_game_in_json, background='yellow', foreground='black').grid(column=3, row=2)
+
+    B1 = Button(root, text="Quit", command=root.destroy).grid(column=2, row=5)
+
+    root.mainloop()
+
+
+def get_gamename(steamy):
+    return steamy.get('name')
+
+
+f = open('steam.json')
+data = json.load(f)
+data_import = []
+for line in data:
+    data_import.append(line)
+
+# return first_game_in_json
+first_game_in_json = data_import[0]['name']
+
+print(data_import[0]['name'])
+
+# sort by gamename, print top
+data_import.sort(key=get_gamename)
+print('yes', data_import[0]['name'])
+
+# reverse sort by gamename, print top
+data_import.sort(key=get_gamename, reverse=True)
+print('yes', data_import[0]['name'])
+
+
+
+# Main Program:
+startup_message()
+GUI_function()
