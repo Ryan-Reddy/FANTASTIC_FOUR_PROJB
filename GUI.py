11db6582bb0db377c4fe02be07d05dc51455f3bd
Léon <leon.jansen@student.hu.nl>
2021-12-18 13:54:40 +0100

    Steamfuctions toegevoegd voor statistiek

--- GUI.py
+++ GUI.py
@@ -1,6 +1,7 @@
 """Gui van de applicatie"""
 from main import *
 from tkinter import *
+from steamFunctions import *
 
 root = Tk()
 root.geometry('1024x720')
@@ -9,9 +10,16 @@ root['bg'] = '#393D47'
 root.wait_visibility(root)
 root.wm_attributes('-alpha', 0.9)
 
-L1 = Label(root, text="First game in list:", background='#393D47', foreground='white').grid(column=2, row=1)
-L2 = Label(root, text=first_game_in_json, background='yellow', foreground='black').grid(column=3, row=2)
+L1 = Label(root, text="First game in list:", background='#393D47', foreground='white').grid(column=1, row=1)
+L2 = Label(root, text=first_game_in_json, background='yellow', foreground='black').grid(column=2, row=1)
 
-B1 = Button(root, text="Quit", command=root.destroy).grid(column=2, row=5)
+L3 = Label(root, text="Average game price:", background='#393D47', foreground='white').grid(column=1, row=2)
+L4 = Label(root, text=average_game_price(), background='yellow', foreground='black').grid(column=2, row=2)
+
+L5 = Label(root, text="First game developer:", background='#393D47', foreground='white').grid(column=1, row=3)
+L6 = Label(root, text=list_first_game_developers(), background='yellow', foreground='black').grid(column=2, row=3)
+
+
+B1 = Button(root, text="Quit Steam Dashboard", command=root.destroy).grid(column=1, row=4)
 
 root.mainloop()
---
+++ steamFunctions.py
@@ -0,0 +1,54 @@
+# Functie voor berekenen gemiddelde prijs van alle games.
+def average_game_price():
+    # Importeer json om steam.json correct uit te lezen.
+    import json
+
+    # Het json bestand uitlezen en opslaan als variable.
+    source = open("steam.json")
+    data = json.load(source)
+
+    # Lees elk spel uit het bestand in en sla het aantal spellen en de totale prijs op.
+    count = 0
+    total = 0
+    for i in data:
+        count += 1
+        total += int(i["price"])
+
+    # Bepaal de gemiddelde prijs van alle spellen en geeft deze waarde terug als getal met 2 decimalen.
+    average = total / count
+    #average = "{:.2f}".format(total/count)
+    formatting = "{average_price:.2f}"
+    return formatting.format(average_price=average)
+
+
+# Functie voor ophalen van alle game developers.
+def list_game_developers():
+    # Importeer json om steam.json correct uit te lezen.
+    import json
+
+    # Het json bestand uitlezen en opslaan als variable.
+    source = open("steam.json")
+    data = json.load(source)
+
+    # Lees alle developers uit het bestand in en sla de namen op, geeft deze namen terug als resultaat.
+    developers = set()
+    for i in data:
+        x = str(i["developer"])
+        developers.add(x)
+
+    return developers
+
+
+# Functie voor ophalen van alle game developers.
+def list_first_game_developers():
+    # Importeer json om steam.json correct uit te lezen.
+    import json
+
+    # Het json bestand uitlezen en opslaan als variable.
+    source = open("steam.json")
+    data = json.load(source)
+
+    # Lees de developer(s) uit het bestand in van de eerste game en sla deze op, geeft dit terug als resultaat.
+    x = data[0]
+    developers = x["developer"]
+    return developers
