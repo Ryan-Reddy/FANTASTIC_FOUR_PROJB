112df84a3308d6dbcd448a695c9957f4a6c427c0
Ryan Reddy <90551370+klugook@users.noreply.github.com>
2022-01-13 15:14:23 +0100

    About knop toegevoegd (readme file screen) + Design kleurkeus variabelen toegevoegd aan GUI

--- GUI.py
+++ GUI.py
@@ -45,14 +45,18 @@ def openNewWindow_readme():
 # Label van eerste spel in lijst:
 L1 = Label(root, text="First game in list:", background=back_color, foreground=font_color).grid(column=1, row=1)
 L2 = Label(root, text=first_game_in_json, background='yellow', foreground='black').grid(column=2, row=1)
+
 # label van gemiddelde prijs van de games:
 L3 = Label(root, text="Average game price:", background=back_color, foreground=font_color).grid(column=1, row=2)
 L4 = Label(root, text=average_game_price(), background='yellow', foreground='black').grid(column=2, row=2)
+
 # Label van eerste game dev in de lijst:
 L5 = Label(root, text="First game developer:", background=back_color, foreground=font_color).grid(column=1, row=3)
 L6 = Label(root, text=list_first_game_developers(), background='yellow', foreground='black').grid(column=2, row=3)
+
 # Knop om programma te eindigen
 B1 = Button(root, text="Quit Steam Dashboard", command=root.destroy).grid(column=1, row=4)
+
 # Knop voor about
 B2 = Button(root, text="About", command=openNewWindow_readme).grid(column=2, row=4)
 
